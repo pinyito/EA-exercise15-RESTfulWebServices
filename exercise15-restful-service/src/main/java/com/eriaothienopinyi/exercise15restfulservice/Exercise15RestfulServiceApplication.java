76392baaff18cb07e89f61f0e69d7f929375e505
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2022-05-10 15:24:49 -0500

    Set up the Spring Boot Project.

---
+++ exercise15-restful-service/src/main/java/com/eriaothienopinyi/exercise15restfulservice/Exercise15RestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eriaothienopinyi.exercise15restfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise15RestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise15RestfulServiceApplication.class, args);
+	}
+
+}
---
+++ exercise15-restful-service/src/test/java/com/eriaothienopinyi/exercise15restfulservice/Exercise15RestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eriaothienopinyi.exercise15restfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise15RestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
